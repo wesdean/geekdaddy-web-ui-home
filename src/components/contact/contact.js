98663b8867972d96ae3d1002877b61cbf09e1af5
Wes Dean <wes@geekdaddydean.com>
2019-03-07 07:32:11 -0600

    Added Contact component

---
+++ src/components/contact/contact.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './contact.scss';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="Contact">
+        <div className="Contact-section-label">Contact</div>
+        <div className="Contact-info">
+          <div>Wes Dean</div>
+          <div><a href="mailto:support@geekdaddydean.com">support@geekdaddydean.com</a></div>
+          <div><a href="tel:479-310-5756">479-310-5756</a></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contact;
--- src/components/scroll-home/scroll-home.js
+++ src/components/scroll-home/scroll-home.js
@@ -2,13 +2,18 @@ import React, {Component} from 'react';
 import AboutMe from "../about-me/about-me";
 import './scroll-home.scss';
 import Footer from "../footer/footer";
+import Contact from "../contact/contact";
 
 class ScrollHome extends Component {
   render() {
     return (
       <div className="ScrollHome">
         <hr className="ScrollHome-spacer"/>
-        <AboutMe/>
+        <div className="ScrollHome-content">
+          <AboutMe/>
+          <hr className="ScrollHome-spacer"/>
+          <Contact/>
+        </div>
         <hr className="ScrollHome-spacer"/>
         <Footer/>
       </div>
